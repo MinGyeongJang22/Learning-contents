6d88edfb05b541a4012d5f6d0d3e2db15780c3a9
MinGyeongJang <masibirth1019@gmail.com>
2024-03-24 16:32:27 +0900

    깃허브 테스트입니다. This is Github Test.

---
+++ src/sample1/Test01.java
@@ -0,0 +1,14 @@
+package sample1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello.");
+		System.out.println("This is Sample.");
+		
+		
+		
+	}//main
+	
+}
